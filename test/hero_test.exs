851e50f83ad3f54f88a3d52456e13d239c66d21d
Guy Royse <guy@guyroyse.com>
2016-01-16 20:54:30 -0500

    whitespace

--- test/hero_test.exs
+++ test/hero_test.exs
@@ -6,18 +6,15 @@ defmodule HeroTest do
   end
 
   ## name
-
   test "it has a default name of empty string", context do
     assert Hero.name(context[:subject]) == ""
   end
-
   test "it has a settable name", context do
     {:ok, hero} = Hero.name(context[:subject], "Bob the Barbarian")
     assert Hero.name(hero) == "Bob the Barbarian"
   end
 
   ## alignment
-
   test "it has a default alignment of neutral", context do
     assert Hero.alignment(context[:subject]) == :neutral
   end
@@ -44,13 +41,11 @@ defmodule HeroTest do
   end
 
   ## armor class
-
   test "it has default armor class of 10", context do
     assert Hero.armor_class(context[:subject]) == 10
   end
 
   ## hit points
-
   test "it has default hit points of 5", context do
     assert Hero.hit_points(context[:subject]) == 5
   end
@@ -61,7 +56,6 @@ defmodule HeroTest do
   end
 
   ## alive?
-
   test "it is alive when undamaged", context do
     assert Hero.alive?(context[:subject]) == true
   end
@@ -80,5 +74,5 @@ defmodule HeroTest do
     {:ok, hero} = Hero.damage(context[:subject], 10)
     assert Hero.alive?(hero) == false
   end
-  
+
 end
