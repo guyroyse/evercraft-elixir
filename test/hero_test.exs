3379233c15f3aff72e7a7f1655e3cbdc3cd920f5
Guy Royse <guy@guyroyse.com>
2016-01-17 20:55:33 -0500

    whitespace

--- test/hero_test.exs
+++ test/hero_test.exs
@@ -42,21 +42,18 @@ defmodule HeroTest do
       assert Hero.ability_score(context[:subject], ability) == 10
     end)
   end
-
   test "it has settable abilities", context do
     Enum.each([:str, :dex, :con], fn(ability) ->
       {:ok, hero} = Hero.ability_score(context[:subject], ability, 15)
       assert Hero.ability_score(hero, ability) == 15
     end)
   end
-
   test "it complains about out of range abilities", context do
     Enum.each([:str, :dex, :con], fn(ability) ->
       {:error, reason} = Hero.ability_score(context[:subject], ability, 25)
       assert reason == "invalid score"
     end)
   end
-
   test "it has expected ability modifiers", context do
     Enum.each([:str, :dex, :con], fn(ability) ->
       {:ok, hero} = Hero.ability_score(context[:subject], ability, 15)
