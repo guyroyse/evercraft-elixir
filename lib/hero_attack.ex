e8f4837a1e7185746ff802cd8b79b13c475971eb
Guy Royse <guy@guyroyse.com>
2016-01-27 17:44:28 -0500

    refactored Hero.Attack

--- lib/hero_attack.ex
+++ lib/hero_attack.ex
@@ -1,30 +1,30 @@
 defmodule Hero.Attack do
 
   def modifier(hero) do
-    base_modifier(hero) + ability_modifier(hero)
+    Hero.Attack.Modifier.base(hero) + Hero.Attack.Modifier.ability(hero)
   end
 
   def damage(hero) do
-    max(1, base_damage(hero))
+    max(1, base_damage_plus_ability_modifier(hero))
   end
 
   def critical_damage(hero) do
-    max(1, critical_mulitplier(hero) * base_damage(hero))
+    max(1, Hero.Attack.Damage.critical_mulitplier(hero) * base_damage_plus_ability_modifier(hero))
   end
 
-  defp base_modifier(hero) do
-    trunc(Hero.Experience.level(hero) * base_modifier_per_level(hero))
+  defp base_damage_plus_ability_modifier(hero) do
+    Hero.Attack.Damage.base(hero) + Hero.Attack.Damage.ability_modifier(hero)
   end
 
-  defp base_modifier_per_level(hero) do
-    case Hero.class(hero) do
-      :fighter ->  1
-      :monk -> 2 / 3
-      _ -> 1 / 2
-    end
+end
+
+defmodule Hero.Attack.Modifier do
+
+  def base(hero) do
+    trunc(Hero.Experience.level(hero) * attack_base_modifier_per_level(hero))
   end
 
-  defp ability_modifier(hero) do
+  def ability(hero) do
     case Hero.class(hero) do
       :rogue -> Hero.Ability.modifier(hero, :dex)
       :monk -> Hero.Ability.modifier(hero, :str) + max(0, Hero.Ability.modifier(hero, :wis))
@@ -32,14 +32,30 @@ defmodule Hero.Attack do
     end
   end
 
-  defp base_damage(hero) do
+  defp attack_base_modifier_per_level(hero) do
     case Hero.class(hero) do
-      :monk -> 3 + Hero.Ability.modifier(hero, :str)
-      _ -> 1 + Hero.Ability.modifier(hero, :str)
+      :fighter ->  1
+      :monk -> 2 / 3
+      _ -> 1 / 2
     end
   end
 
-  defp critical_mulitplier(hero) do
+end
+
+defmodule Hero.Attack.Damage do
+
+  def base(hero) do
+    case Hero.class(hero) do
+      :monk -> 3
+      _ -> 1
+    end
+  end
+
+  def ability_modifier(hero) do
+    Hero.Ability.modifier(hero, :str)
+  end
+
+  def critical_mulitplier(hero) do
     case Hero.class(hero) do
       :rogue -> 3
       _ -> 2
